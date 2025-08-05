af93feb3947aaaf9d255b897790f4d7ad54fd4d9
mixka <pikopavel@gmail.com>
2025-08-05 21:24:01 +0300

    server.js update

--- src/back/server.js
+++ src/back/server.js
@@ -3,3 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
