44e6ff6f6c47bc30fbe7d79749ae0d784adfbe4d
mixka <pikopavel@gmail.com>
2025-08-06 22:00:01 +0300

    404 pages

--- src/page/NotFoundPage/NotFound.tsx
+++ src/page/NotFoundPage/NotFound.tsx
@@ -1,8 +1,14 @@
+import logo from "./img/logo.png"
+import s from "./notfound.module.css"
 
 export const NotFoundPage = () => {
     return(
         <div>
-            404
+            <div className={s.error}>
+                <h1>4</h1> 
+                <img src={logo} className={s.logo} alt="0" /> 
+                <h1>4</h1>
+            </div>
         </div>
     )
 }
